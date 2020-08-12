6ebe0fcee4fe8e02ed8bea012b0455abd92025d1
AndySong <andylauszp@gmail.com>
2020-08-12 12:40:00 +1200

    oop-classes

---
+++ src/OOP/Classes/AchievementBadge.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\OOP\Classes;
+
+class AchievementBadge
+{
+    public $title;
+    public $description;
+    public $points;
+
+    public function awardTo($user)
+    {
+    }
+}
